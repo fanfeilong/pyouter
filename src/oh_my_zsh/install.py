2d0d5e227e1f738d1fdf565269f13d08098734bb
Mars Liu <mars.liu@outlook.com>
2021-11-11 00:50:21 +0800

    write plugins into .zshrc if not exists

--- src/oh_my_zsh/install.py
+++ src/oh_my_zsh/install.py
@@ -1,6 +1,7 @@
 import string
 import os
 import os.path
+import re
 
 
 def install(config, options):
@@ -16,11 +17,31 @@ _pipeline() {
 compdef _pipeline python actions.py
 """)
     home = os.getenv("HOME")
-    plugin_dir = os.path.join(home, ".oh-my-zsh/plugins/tasks-router")
-    plugin_path = os.path.join(plugin_dir, "tasks-router.plugin.zsh")
+    plugin_name = "tasks-router"
+    plugin_dir = os.path.join(home, ".oh-my-zsh/plugins", plugin_name)
+    plugin_path = os.path.join(plugin_dir, plugin_name + ".plugin.zsh")
+    zshrc = os.path.join(home, ".zshrc")
+    regex = re.compile(r"\nplugins=\(.*?\)")
+
     if not os.path.exists(plugin_dir):
         os.mkdir(plugin_dir)
 
+    with open(zshrc, "r") as f:
+        zshrc_content = f.read()
+
+    m = regex.search(zshrc_content)
+    if m is None:
+        zshrc_content = zshrc_content + f"\nplugins=({plugin_name})\n"
+    else:
+        plugins_setting = m.group(0)
+        if "tasks-router" not in plugins_setting:
+            plugins_setting = plugins_setting[:-1] + f" {plugin_name}" + plugins_setting[-1]
+            prev, post = regex.split(zshrc_content)
+            zshrc_content = prev + plugins_setting + post
+
     with open(plugin_path, "w+") as f:
         f.write(command.substitute(script="main.py"))
+
+    with open(zshrc, "w+") as f:
+        f.write(zshrc_content)
     print("success")

1117a7fd9e4178c7e4744701a37b98e05a37bf9c
feilong <fanfeilong@outlook.com>
2024-07-11 19:16:41 +0800

    improve code

--- src/pyouter/app.py
+++ src/pyouter/app.py
@@ -1,24 +1,47 @@
 
+from typing import Any
 from pyouter.default import create_parser
 from pyouter.errors import NotInit
 from pyouter.router import Router
 
-
 class App(object):
-    def __init__(self, config=None, parser=None):
-        opt_parser = create_parser("tasks router runner") if parser is None else parser
-        self.options = opt_parser.parse_args()
-        self.config = {} if config is None else config
+    def __init__(self, config=None):
+        opt_parser = create_parser("tasks router runner")
+        self.opt_parser = opt_parser
+        self.options = None
+        
+        if config is None:
+            self.config = {}
+        elif type(config)==type(''):
+            import json
+            with open(config,'r') as f:
+                self.config = json.load(f)
+        elif type(config)==type({}):
+            self.config = config
+        else:
+            raise NotInit("config type unknonw")
+            
         self.router: Router
-
+    
     def use(self, router: Router):
         self.router = router
-        self.router.context(self.config, self.options)
+        return self
+    
+    def option(self, *args: Any, **kwds: Any):
+        self.opt_parser.add_argument(*args, **kwds)
         return self
 
     def run(self):
         if self.router is None:
             raise NotInit("self.router in App")
+        
+        if self.config is None:
+            raise NotInit("self.config in App")
+        
+        if self.options is None:
+            self.options = self.opt_parser.parse_args()
+            
+        self.router.context(self.config, self.options)
 
         if self.options.tasks:
             for task in self.router.tasks():
